5d16727d82f4d24aca46aa54947d006ae36a1663
Erick Amoedo <erick.amoedo@live.com>
2021-07-15 13:14:49 -0300

    fix Warning de server

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
 const url = "mongodb://localhost:27017/todo";
-mongoose.connect(url, { useNewUrlParser: true });
+
+// Ajustei o erro de depreciacao
+// https://stackoverflow.com/questions/57895175/server-discovery-and-monitoring-engine-is-deprecated
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = mongoose;
